74f6b39019c53f3084ac55f8e31798fa7dcc0fd5
Brandy Mello <brandyamello@gmail.com>
2019-07-14 11:35:09 -0600

    Start Game and Player class framework

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,20 @@
+import data from './data';
+import Player from './Player';
+import Round from './Round';
+
 class Game {
   constructor() {
-    
+    this.data = data;
+    this.round = 0;
+    this.players = [];
   }
+
+  startGame() {
+    let player1 = new Player(1, 'Brandy');
+    let player2 = new Player(2, 'Lacy');
+    let player3 = new Player(3, 'Julian');
+    let round = new Round
+  }
+
+
 }
---
+++ src/Player.js
@@ -0,0 +1,8 @@
+import Puzzle from '.Puzzle';
+import Round from '.Round';
+
+class Round {
+  constructor(players,) {
+
+  }
+}
