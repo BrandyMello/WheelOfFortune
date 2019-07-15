10cb2ea3fa490d1ef80c19e9d57f925fcfcd839d
dawnlunacy <alanturing@example.com>
2019-07-15 16:43:51 -0600

    Add pseudocoding to game, wheel, and turn classes

--- src/Turn.js
+++ src/Turn.js
@@ -1,8 +1,10 @@
 //  ** Spin Wheel **
 
- // When the player spins the wheel the Wheel class will run a method to randomly make a selection from within the wheel array.
+ // When the player spins the wheel the Wheel class will run the findWheelPrize method to randomly make a selection from within the wheel array.
  // The selection will be stored as a property on the Wheel class as the value of the corresponding index.
- // A method will be invoked to determine if the player has landed on bankrupt or lose a turn. If they have, then the turn property that determines if 
+
+
+ // A method will be invoked to determine if the player has landed on bankrupt or lose a turn. If they have, then the turn property (that determines if 
  // the player has a valid turn will turn to false and their turn will end.
  // If the selection is a number then the turn property will be assigned to true.
  // There is a method in the Player class that will validate if it is still the players turn.
@@ -12,6 +14,9 @@
 //  
 // 
 // 
-// 
-// 
+
+// spinWheel will be called from the Turn class when a player clicks the spin button
+//and that will really just call the findWheelPrize method inside of Wheel Class
+
+
 // 
